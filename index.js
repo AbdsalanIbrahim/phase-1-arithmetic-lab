b86a252412ca408d286ffda8e4c7cf8c6e6fed71
Abdisalan Ibrahim <crgmelo22@gmail.com>
2024-10-01 14:49:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // Result: 62
 
-// Write your code here
+let random = Math.floor(Math.random() * 100) + 1; // Random number between 1 and 100
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4; // Result: 4
+
+let max = Math.max(1, 5, 10, 20); // Result: 20
+
